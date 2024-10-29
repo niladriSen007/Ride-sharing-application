2e50a53785b275b73c3dcb8333076a6833489855
Niladri <niladrisengit12@gmail.com>
2024-10-29 21:47:15 +0530

    request-ride-creation-complete

--- src/main/java/com/niladri/RideSharingApplication/repository/driver/DriverRepository.java
+++ src/main/java/com/niladri/RideSharingApplication/repository/driver/DriverRepository.java
@@ -11,13 +11,13 @@ import java.util.List;
 @Repository
 public interface DriverRepository extends JpaRepository<DriverModel, Long> {
 
-	@Query(value = "select d,ST_Distance(d.current_location,:pickupLocation) as distance from DriverModel d " +
+	@Query(value = "select d.*,ST_Distance(d.current_location,:pickupLocation) as distance from driver_model d " +
 			"where d.available = true and ST_DWithin(d.current_location, :pickupLocation, 10000) " +
 			"order by distance limit 10",nativeQuery = true)
 	List<DriverModel> findMatchingDriversNearby(Point pickupLocation);
 
 
-	@Query(value = "select d,ST_Distance(d.current_location,:pickupLocation) as distance from DriverModel d " +
+	@Query(value = "select d.*,ST_Distance(d.current_location,:pickupLocation) as distance from driver_model d " +
 			"where d.available = true and ST_DWithin(d.current_location, :pickupLocation, 15000) " +
 			"order by d.rating desc limit 10",nativeQuery = true)
 	List<DriverModel> findMatchingDriversNearbyAndHighestRated(Point pickupLocation);
